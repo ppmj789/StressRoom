28cd5dd6cd132976d641373d3d25de8eac3afe4f
wkdthf21 <ppmj789@naver.com>
2018-12-09 20:35:11 +0900

    stage 구성

---
+++ StressRoom/Assets/Scripts/MainCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+
+    public GameObject player;
+
+    public float offsetX = 0f;
+    public float offsetY = 25f;
+    public float offsetZ = -35f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
